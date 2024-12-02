319ad8a782d31ebab8bb29cc2ca0057e52c4e831
Anmol Sharma <anmolsharma0234@gmail.com>
2024-12-02 11:34:15 +0530

    feat: week 4 assignment
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

---
+++ test.sh
@@ -0,0 +1,5 @@
+pip3 install -r ./requirements.txt
+jupyter nbconvert --execute ./chapter-6/Chapter6.ipynb --to python
+python3 chapter-6/Chapter6.py
+jupyter nbconvert --execute ./chapter-7/Chapter7.ipynb --to python
+python3 chapter-7/Chapter7.py
